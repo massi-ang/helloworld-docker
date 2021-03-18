478135f3a69f4984087b1e5662d610daa0a19fab
Massimiliano Angelino <angmas@amazon.com>
2021-03-18 15:53:33 +0100

    world

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 import time
 while True:
-  print(f"Hello, now is {time.strftime('%Y-%m-%dT%H:%M:%S',time.localtime())}")
+  print(f"Hello world, now is {time.strftime('%Y-%m-%dT%H:%M:%S',time.localtime())}")
   time.sleep(5)
